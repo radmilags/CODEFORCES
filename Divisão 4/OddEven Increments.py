65532d145d34a9fc6212cd0f3f37c838d9d03862
Radmila Gama <milaejared@outlook.com>
2022-05-20 22:12:29 -0300

    Soluções - refazer

---
+++ Divisão 4/OddEven Increments.py
@@ -0,0 +1,10 @@
+n = int(input())
+while n <= n:
+    size = int(input())
+    s = input().split(" ")
+    if size % 2 != 0:
+        n1 = 1
+        while n1 < size:
+            
+        n1 += 1
+    n += 1
